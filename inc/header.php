9471f1e9c2afe4ce91d2091d06eb196e4db3364c
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-01-25 13:17:22 +0100

    Initial commit 2

---
+++ inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <title>TITRE</title>
+
+</head>
+<body>
+
+<header>
+
+
+</header>
+
